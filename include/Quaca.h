d81bd569b49058af94cd41e3b4f14aff5c53a6e1
Christoph Egerland <egerlanc@physik.hu-berlin.de>
2019-11-06 14:21:37 +0100

    Updated header.

--- include/Quaca.h
+++ include/Quaca.h
@@ -1,6 +1,8 @@
 #ifndef QUACA_H
 #define QUACA_H
 
+#include "../src/GreensTensor/Permittivity/Permittivity.h"
+#include "../src/GreensTensor/Permittivity/PermittivityDrude.h"
 #include "../src/Polarizability/Polarizability.h"
 #include "../src/Polarizability/PolarizabilityBath.h"
 #include "../src/Polarizability/MemoryKernel/MemoryKernel.h"
