ba382943e2525ba8a171d0427988fd9fad0bfd49
Christoph Egerland <egerlanc@physik.hu-berlin.de>
2019-11-06 14:20:26 +0100

    Added permittivity class.

---
+++ src/GreensTensor/Permittivity/Permittivity.h
@@ -0,0 +1,23 @@
+#ifndef PERMITTIVITY_H
+#define PERMITTIVITY_H
+
+#include <complex>
+
+//! An abstract permittivity class
+/*!
+* This is an abstract class for permittivities
+* All permittivities should return a complex number, given a real frequency as input.
+* Being susceptibilities, all permittivities should obey the crossing relation.
+*/
+class Permittivity
+{
+public:
+  
+  /*!
+  * Return the permittivity given at frequency omega
+  * @param omega Frequency
+  */
+  virtual std::complex<double> epsilon(double omega) =0;
+};
+
+#endif // PERMITTIVITY_H
