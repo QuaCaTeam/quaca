3459083761b223127c229b74d8e2d97429f91436
Marty <marty@physik.hu-berlin.de>
2020-01-06 15:37:47 +0100

    Implemented a first GreensTensorPlate test

--- test/GreensTensor/test_GreensTensorPlate.cpp
+++ test/GreensTensor/test_GreensTensorPlate.cpp
@@ -4,24 +4,15 @@
 #include "catch.hpp"
 #include "Quaca.h"
 
-TEST_CASE("Vacuum Greens Tensor works properly")
+TEST_CASE("Plate Greens Tensor works properly")
 {
   struct Options_GreensTensor opts;
   opts.fancy_I=true;
   opts.omega = 3.0;
+  opts.kvev = { 10.0 , NAN };
 
-  GreensTensorVacuum Greens(0.01,1e3);
+  GreensTensorPlate Greens(0.01 , 1e-1, 1e3, "../data/test_files/PermittivityDrude.ini");
   opts.class_pt = &Greens;
 
-  cx_mat::fixed<3,3> test(fill::zeros);
-
-  Greens.integrate_k_1d(test, opts);
-
-  double analytic_result_prefactor = 2./3.*pow(opts.omega,3)/pow(1-pow(Greens.get_v(),2),2);
-
-  std::cout << test << std::endl;
-  REQUIRE( Approx(test(0,0).real()).epsilon(1E-4) == analytic_result_prefactor );
-  REQUIRE( Approx(test(1,1).real()).epsilon(1E-4) == analytic_result_prefactor*(1+pow(Greens.get_v(),2))/(1-pow(Greens.get_v(),2)));
-  REQUIRE( Approx(test(2,2).real()).epsilon(1E-4) == analytic_result_prefactor*(1+pow(Greens.get_v(),2))/(1-pow(Greens.get_v(),2)));
-
+  std::cout << Greens.integrate_k_2d( 1, opts) << std::endl;
 };
