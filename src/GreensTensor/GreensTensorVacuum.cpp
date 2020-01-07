d196062df01ef37d15cc6447ac101a5292c8f690
simonmh <simon.hermann@posteo.de>
2020-01-07 11:13:38 +0100

    All remaining test of the PDF relations_and_tests are implemented for the VacuumsGreensTensor

--- src/GreensTensor/GreensTensorVacuum.cpp
+++ src/GreensTensor/GreensTensorVacuum.cpp
@@ -14,16 +14,16 @@ GreensTensorVacuum::GreensTensorVacuum(double v, double beta)
 void GreensTensorVacuum::calculate_tensor(cx_mat::fixed<3,3>& GT, vec::fixed<2> kvec, double omega)
 {
   // calculating the solely the imaginary part of the free Green tensor
-  double pre, kx, ky, k2, omega2;
-  kx = kvec(0);
-  ky = kvec(1);
-  k2 = kx*kx + ky*ky;
-  omega2 = omega*omega;
-  pre = 1.0/(2*M_PI*sqrt(omega2 - k2));
+  double pre, k_x, k_y, k_quad, omega_quad;
+  k_x = kvec(0);
+  k_y = kvec(1);
+  k_quad = k_x*k_x + k_y*k_y;
+  omega_quad = omega*omega;
+  pre = 1.0/(2*M_PI*sqrt(omega_quad - k_quad));
   GT.zeros();
-  GT(0,0) = pre*(omega2 - kx*kx);
-  GT(1,1) = pre*(omega2 - ky*ky);
-  GT(2,2) = pre*k2;
+  GT(0,0) = pre*(omega_quad - k_x*k_x);
+  GT(1,1) = pre*(omega_quad - k_y*k_y);
+  GT(2,2) = pre*k_quad;
 };
 
 void GreensTensorVacuum::integrate_k_2d(cx_mat::fixed<3,3>& GT, Options_GreensTensor opts)
--- test/GreensTensor/test_GreensTensorVacuum.cpp
+++ test/GreensTensor/test_GreensTensorVacuum.cpp
@@ -6,16 +6,52 @@
 
 TEST_CASE("Vacuum Greens Tensor works properly")
 {
+
+  GreensTensorVacuum Greens(0.01,1e3);
+  /*!
+   * Some basic relations any Green's tensor should fulfill which can
+   * be found in docs under: Relations_and_tests.pdf
+   */
+  vec::fixed<2> kvec = ones<vec>(2);
+  double omega = 5;
+  cx_mat::fixed<3,3> Greens_lhs(fill::zeros);
+  cx_mat::fixed<3,3> Greens_rhs(fill::zeros);
+  /*!
+   * Crossing relation in frequency domain see eq. [1]
+   */
+  Greens.calculate_tensor(Greens_lhs, -kvec, -omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, omega);
+  REQUIRE(approx_equal(Greens_lhs, trans(conj(Greens_rhs)),"absdiff", 10E-5));
+
+  /*!
+   * Reciprocity, see eq. [6]
+   */
+  Greens.calculate_tensor(Greens_lhs, -kvec, omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, omega);
+  REQUIRE(approx_equal(Greens_lhs, trans(Greens_rhs),"absdiff", 10E-5));
+  
+  /*!
+   * Reality, see eq. [7], due to the divergence of the real part only the imaginary
+   * part is calculated in any case
+   */
+  Greens.calculate_tensor(Greens_lhs, kvec, omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, -omega);
+  REQUIRE(approx_equal(Greens_lhs, Greens_rhs,"absdiff", 10E-5));
+  
+  
+
+  /*!
+   * Test the integration routine of the vacuums Green's tensor, the analytical results
+   * can be found in the docs under: VacuumGreen.pdf, see eq. [21]
+   */
   struct Options_GreensTensor opts;
   opts.fancy_I=true;
   opts.omega = 3.0;
-
-  GreensTensorVacuum Greens(0.01,1e3);
   opts.class_pt = &Greens;
 
   cx_mat::fixed<3,3> test(fill::zeros);
  
-  Greens.integrate_k_1d(test, opts);
+  Greens.integrate_k_1d(test,opts);
 
   double analytic_result_prefactor = 2./3.*pow(opts.omega,3)/pow(1-pow(Greens.get_v(),2),2);
 
