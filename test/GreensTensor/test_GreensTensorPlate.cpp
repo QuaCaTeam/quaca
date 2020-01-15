5d7c7fef360674854758c9b7619f78eb61a38ff9
Marty <marty@physik.hu-berlin.de>
2020-01-15 12:06:22 +0100

    Inserted free space Green's tensor test into plate version

--- test/GreensTensor/test_GreensTensorPlate.cpp
+++ test/GreensTensor/test_GreensTensorPlate.cpp
@@ -21,3 +21,61 @@ TEST_CASE("Plate Greens Tensor works properly")
 //  std::cout << Greens.integrand_k_2d( 1., &opts) << std::endl;
   std::cout << Greens.integrand_k_1d( 1., &opts) << std::endl;
 };
+
+TEST_CASE("Vacuum Greens Tensor works properly")
+{
+
+  GreensTensorVacuum Greens(0.01,1e3);
+  /*!
+   * Some basic relations any Green's tensor should fulfill which can
+   * be found in docs under: Relations_and_tests.pdf
+   */
+  vec::fixed<2> kvec = ones<vec>(2);
+  double omega = 5;
+  cx_mat::fixed<3,3> Greens_lhs(fill::zeros);
+  cx_mat::fixed<3,3> Greens_rhs(fill::zeros);
+  /*!
+   * Crossing relation in frequency domain see eq. [1]
+   */
+  Greens.calculate_tensor(Greens_lhs, -kvec, -omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, omega);
+  REQUIRE(approx_equal(Greens_lhs, trans(conj(Greens_rhs)),"absdiff", 10E-5));
+
+  /*!
+   * Reciprocity, see eq. [6]
+   */
+  Greens.calculate_tensor(Greens_lhs, -kvec, omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, omega);
+  REQUIRE(approx_equal(Greens_lhs, trans(Greens_rhs),"absdiff", 10E-5));
+
+  /*!
+   * Reality, see eq. [7], due to the divergence of the real part only the imaginary
+   * part is calculated in any case
+   */
+  Greens.calculate_tensor(Greens_lhs, kvec, omega);
+  Greens.calculate_tensor(Greens_rhs, kvec, -omega);
+  REQUIRE(approx_equal(Greens_lhs, Greens_rhs,"absdiff", 10E-5));
+
+
+
+  /*!
+   * Test the integration routine of the vacuums Green's tensor, the analytical results
+   * can be found in the docs under: VacuumGreen.pdf, see eq. [21]
+   */
+  struct Options_GreensTensor opts;
+  opts.fancy_I=true;
+  opts.omega = 3.0;
+  opts.class_pt = &Greens;
+
+  cx_mat::fixed<3,3> test(fill::zeros);
+
+  Greens.integrate_k_1d(test,opts);
+
+  double analytic_result_prefactor = 2./3.*pow(opts.omega,3)/pow(1-pow(Greens.get_v(),2),2);
+
+  std::cout << test << std::endl;
+  REQUIRE( Approx(test(0,0).real()).epsilon(1E-4) == analytic_result_prefactor );
+  REQUIRE( Approx(test(1,1).real()).epsilon(1E-4) == analytic_result_prefactor*(1+pow(Greens.get_v(),2))/(1-pow(Greens.get_v(),2)));
+  REQUIRE( Approx(test(2,2).real()).epsilon(1E-4) == analytic_result_prefactor*(1+pow(Greens.get_v(),2))/(1-pow(Greens.get_v(),2)));
+
+};
