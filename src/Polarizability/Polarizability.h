f25174057f75ccb8bfc9b92d85497fa160a20c4e
Marty <marty@physik.hu-berlin.de>
2020-02-02 21:48:41 +0100

    fishy Pola

--- src/Polarizability/Polarizability.h
+++ src/Polarizability/Polarizability.h
@@ -11,44 +11,33 @@
 using namespace arma;
 
 //! a struct with the integration options
-struct Options_Polarizabiliy;
+struct Options_Polarizability;
 
 //! An abstract polarizability class
-/*!
- * This is an abstract polarizability class.
- * All polarizabilities should return a 3 by 3 matrix, given a real frequency as
- * input. The only two child class of this will be 1) a polarizability where the
- * particle has no interval bath 2) a polarizability where the particle has an
- * internal bath
- */
 class Polarizability {
 protected:
-  /*! resonance frequency */
-  double omega_a;
-
-  /*! static polarizability */
-  double alpha_zero;
-
-  /*! green's tensor */
-  GreensTensor *greens_tensor;
+  double omega_a;              // resonance frequency
+  double alpha_zero;           // vacuum polarizability
+  GreensTensor *greens_tensor; // Green's tensor
 
 public:
-  /*!
-   * Returns the polarizability tensor as a 3x3 matrix when given a real
-   * frequency as input.
-   * @param omega Frequency
-   */
-  virtual void calculate(cx_mat::fixed<3, 3> &alpha, double omega) = 0;
+  // constructors
+  Polarizability(double omega_a, double alpha_zero,
+                 GreensTensor *greens_tensor);
+  Polarizability(std::string input_file);
 
-  /*!
-   * Getter method for the resonance frequency.
-   */
-  double get_omega_a();
+  // calculate the polarizability tensor
+  virtual void calculate_tensor(cx_mat::fixed<3, 3> &alpha,
+                                Options_Polarizability opts) = 0;
 
-  /*!
-   * Getter method for the static polarizability.
-   */
-  double get_alpha_zero();
+  // integration over omega
+  double integrate_omega(Options_Polarizability opts, double omega_min,
+                         double omega_max, double relerr, double abserr);
+  static double integrand_omega(double omega, void *opts);
+
+  // getter functions
+  double get_omega_a() { return this->omega_a; };
+  double get_alpha_zero() { return this->alpha_zero; };
 };
 
 // A struct for integration options
@@ -56,13 +45,12 @@ struct Options_Polarizability {
   // Different options for the integrand
   bool fancy_R = false;
   bool fancy_I = false;
-  bool fancy_I_kv = false;
-  bool fancy_I_temp = false;
-  bool fancy_I_kv_temp = false;
-  // Indices of the 3x3 polarizability tensor
-  arma::vec::fixed<2> indices = {-1, -1};
-  // Value of omega for the integration of the k-Variables
+
   double omega = NAN;
+
+  // Indices of the 3x3 polarizability tensor
+  vec::fixed<2> indices = {-1, -1};
+
   // Pointer to the Polarizability to be able to access the attributes of the
   // class eventhough the integrand is static
   Polarizability *class_pt;
--- src/Polarizability/PolarizabilityBath.cpp
+++ src/Polarizability/PolarizabilityBath.cpp
@@ -1,48 +1,29 @@
-// ini parser
-#include <boost/property_tree/ini_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-namespace pt = boost::property_tree;
-
-#include "MemoryKernel/MemoryKernelFactory.h"
 #include "PolarizabilityBath.h"
 
-PolarizabilityBath::PolarizabilityBath(double a, double b, MemoryKernel *mu) {
-  // set parameters
-  this->omega_a = a;
-  this->alpha_zero = b;
-
-  // set memory kernel
-  this->memorykernel = mu;
-}
-
-PolarizabilityBath::PolarizabilityBath(std::string input_file) {
-  // Create a root
-  pt::ptree root;
-
-  // Load the ini file in this ptree
-  pt::read_ini(input_file, root);
-
-  // read parameters
-  this->omega_a = root.get<double>("Polarizability.omega_a");
-  this->alpha_zero = root.get<double>("Polarizability.alpha_zero");
-
-  // read memory kernel
-  this->memorykernel = MemoryKernelFactory::create(input_file);
+PolarizabilityBath::PolarizabilityBath(double omega_a, double alpha_zero,
+                                       MemoryKernel *mu,
+                                       GreensTensor *greens_tensor)
+    : Polarizability(omega_a, alpha_zero, greens_tensor) {
+  this->mu = mu;
 };
 
-std::complex<double> PolarizabilityBath::get_mu(double omega) {
-  return this->memorykernel->mu(omega);
+PolarizabilityBath::PolarizabilityBath(std::string input_file)
+    : Polarizability(input_file) {
+  this->mu = MemoryKernelFactory::create(input_file);
 };
 
-void PolarizabilityBath::calculate(cx_mat::fixed<3, 3> &alpha, double omega) {
+void PolarizabilityBath::calculate_tensor(cx_mat::fixed<3, 3> &alpha,
+                                          Options_Polarizability opts) {
   // imaginary unit
   std::complex<double> I(0.0, 1.0);
+
+  double omega = opts.omega;
 
   // calculate diagonal entries
   cx_mat::fixed<3, 3> diag;
   diag.zeros();
   diag(0, 0) = diag(1, 1) = diag(2, 2) =
-      omega_a * omega_a - omega * omega - I * omega * memorykernel->mu(omega);
+      omega_a * omega_a - omega * omega - I * omega * mu->mu(omega);
 
   // calculate integral over green's tensor with fancy R
   cx_mat::fixed<3, 3> greens_R;
@@ -65,4 +46,12 @@ void PolarizabilityBath::calculate(cx_mat::fixed<3, 3> &alpha, double omega) {
   alpha =
       alpha_zero * omega_a * omega_a *
       inv(diag - alpha_zero * omega_a * omega_a * (greens_R + I * greens_I));
+
+  if (opts.fancy_I) {
+    alpha = (alpha - trans(alpha)) /
+            (2.0 * I); // trans is hermitean conjugation in armadillo
+  } else if (opts.fancy_R) {
+    alpha = (alpha + trans(alpha)) /
+            (2.0); // trans is hermitean conjugation in armadillo
+  }
 };
