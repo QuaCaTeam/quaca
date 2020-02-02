fefdca56f74f6c79945137a547ea80bab5acae12
Marty <marty@physik.hu-berlin.de>
2020-02-02 22:41:35 +0100

    added test_pola_bath

--- src/Polarizability/PolarizabilityBath.h
+++ src/Polarizability/PolarizabilityBath.h
@@ -1,26 +1,25 @@
 #ifndef POLARIZABILITYBATH_H
 #define POLARIZABILITYBATH_H
 
-#include <complex>
-#include <cmath>
+#include "MemoryKernel/MemoryKernelFactory.h"
 #include "Polarizability.h"
+#include <cmath>
+#include <complex>
 
-class PolarizabilityBath : public Polarizability
-{
+class PolarizabilityBath : public Polarizability {
 private:
-    // memory kernel and greens tensor needed to calculate alpha
-    MemoryKernel *memorykernel;
+  // memory kernel needed to calculate alpha
+  MemoryKernel *mu;
 
 public:
+  PolarizabilityBath(double omega_a, double alpha_zero, MemoryKernel *mu,
+                     GreensTensor *greens_tensor);
+  PolarizabilityBath(std::string input_file);
 
-    PolarizabilityBath(double a, double b, MemoryKernel *mu);
-    PolarizabilityBath(std::string input_file);
+  void calculate_tensor(cx_mat::fixed<3, 3> &alpha,
+                        Options_Polarizability opts);
 
-    std::complex<double> get_mu(double omega);
-
-    void calculate(cx_mat::fixed<3,3>& alpha, double omega);
-
+  std::complex<double> get_mu(double omega) { return this->mu->mu(omega); };
 };
 
-
-#endif //POLARIZABILITYBATH_H
+#endif // POLARIZABILITYBATH_H
