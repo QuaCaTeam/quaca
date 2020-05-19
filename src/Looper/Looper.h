3bb9b8dea8dd104b73010c642d9b2dafc6a63c17
cheg <egerlanc@physik.hu-berlin.de>
2020-05-19 11:01:04 +0200

    Print_info function for looper

--- src/Looper/Looper.h
+++ src/Looper/Looper.h
@@ -6,7 +6,6 @@
 
 class Looper {
 protected:
-
   double start;              // starting value
   double end;                // end value
   int number_of_steps;       // number of steps
@@ -17,15 +16,21 @@ protected:
 
 public:
   // constructors
-  Looper(double start, double end, int number_of_steps, const std::string &scale);
+  Looper(double start, double end, int number_of_steps,
+         const std::string &scale);
   explicit Looper(const std::string &input_file);
 
   // calculate the the value of quantum friction
-  virtual double calculate_value(int step, std::shared_ptr<Friction> quantum_friction) = 0;
+  virtual double
+  calculate_value(int step,
+                  std::shared_ptr<Friction> quantum_friction) const = 0;
 
   // getter functions
-  int get_steps_total() { return this->number_of_steps; };
-  double get_step(int i) { return this->steps[i]; };
+  int get_steps_total() const { return this->number_of_steps; };
+  double get_step(int i) const { return this->steps[i]; };
+
+  // print info
+  virtual void print_info(std::ostream &stream) const = 0;
 };
 
 #endif // LOOPER_H
--- src/Looper/LooperV.h
+++ src/Looper/LooperV.h
@@ -12,7 +12,10 @@ public:
   LooperV(const std::string &input_file);
 
   // calculate the the value of quantum friction
-  double calculate_value(int step, std::shared_ptr<Friction> quantum_friction);
+  double calculate_value(int step, std::shared_ptr<Friction> quantum_friction) const override;
+
+  // print info
+  void print_info(std::ostream &stream) const override;
 };
 
 #endif // LOOPERV_H
