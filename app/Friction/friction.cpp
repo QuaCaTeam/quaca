c917ad648a180f8c2d4c4d36a6897818aed72648
cheg <egerlanc@physik.hu-berlin.de>
2020-05-20 11:33:30 +0200

    Moved Options struct functionality into friction executable.

--- app/Friction/friction.cpp
+++ app/Friction/friction.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <omp.h>
 
+// program options
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 // json parser
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
@@ -9,13 +13,46 @@ namespace pt = boost::property_tree;
 #include "ProgressBar.hpp"
 #include "Quaca.h"
 
+std::string parameter_file;
+std::string output_file;
+int num_threads = 1;
+
+void read_command_line(int argc, char *argv[]) {
+  /* Read command line options */
+  try {
+    // List all options and their description
+    po::options_description desc("Allowed options");
+    desc.add_options()("help,h", "Help screen")(
+        "file", po::value<std::string>(&(parameter_file)), "Input File")(
+        "threads", po::value<int>(&(num_threads))->default_value(-1),
+        "Number of parallel threads");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // if the help option is given, show the flag description
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      exit(0);
+    }
+
+  } catch (std::exception &e) {
+    std::cerr << "error: " << e.what() << std::endl;
+  }
+
+  /* set according output file */
+  output_file =
+      parameter_file.substr(0, parameter_file.find_last_of('.')) +
+      ".csv";
+}
+
 int main(int argc, char *argv[]) {
   // get file from command line
-  Options opts(argc, argv);
-  std::string parameters = opts.get_parameter_file();
+  read_command_line(argc, argv);
 
   // define looper
-  auto looper = LooperFactory::create(parameters);
+  auto looper = LooperFactory::create(parameter_file);
 
   // array to store all the computed values of the loop
   std::vector<double> friction_data;
@@ -25,36 +62,36 @@ int main(int argc, char *argv[]) {
   ProgressBar progbar(looper->get_steps_total(), 70);
 
   // Check wether the number of threads have been set by the --threads flag
-  if (opts.get_num_threads() == -1) {
+  if (num_threads == -1) {
     // If the --threads flag has not been set, set the number of flags to
     // the maximal value
-    opts.set_num_threads(omp_get_max_threads());
+    num_threads = omp_get_max_threads();
   }
   // Create a paralle region given threads given by the --threads flag
   // we have to create the parallel region already here to ensure,
   // that any thread creates their own instance of quantum_friction
-  std::cout << "Starting parallel region with " << opts.get_num_threads()
+  std::cout << "Starting parallel region with " << num_threads
             << " threads." << std::endl;
-  if (opts.get_num_threads() > omp_get_max_threads()) {
+  if (num_threads > omp_get_max_threads()) {
     std::cout
         << "There are not enough avaiable threads. Maximal avaiable threads: "
         << omp_get_max_threads() << std::endl;
     std::cout << "Aborting calculation" << std::endl;
     exit(0);
   }
-#pragma omp parallel num_threads(opts.get_num_threads())
+#pragma omp parallel num_threads(num_threads)
   {
 
     // Create a root
     pt::ptree root;
 
     // Load the json file in this ptree
-    pt::read_json(parameters, root);
+    pt::read_json(parameter_file, root);
 
     double relerr_omega = root.get<double>("Friction.relerr_omega");
 
     // define needed quantities
-    auto polarizability = std::make_shared<Polarizability>(parameters);
+    auto polarizability = std::make_shared<Polarizability>(parameter_file);
     auto powerspectrum = std::make_shared<PowerSpectrum>(
         polarizability->get_greens_tensor(), polarizability);
     auto quant_friction =
@@ -79,7 +116,7 @@ int main(int argc, char *argv[]) {
 
   // define output file
   std::ofstream file;
-  file.open(opts.get_output_file());
+  file.open(output_file);
 
   // write the data to the file
   double step;
