346de4185bd6c1b75c33944cc6046b03a4291310
Simon <simon.hermann@posteo.de>
2020-07-01 15:43:47 +0200

    Updated the Friction Class constructor with input file, such that only one instance of polarizability and GreensTensor is created.

--- src/Friction/Friction.cpp
+++ src/Friction/Friction.cpp
@@ -21,9 +21,9 @@ Friction::Friction(const std::string &input_file) {
   this->relerr_omega = root.get<double>("Friction.relerr_omega");
 
   // read greens tensor
-  this->greens_tensor = GreensTensorFactory::create(input_file);
-  this->polarizability = std::make_shared<Polarizability>(input_file);
   this->powerspectrum = std::make_shared<PowerSpectrum>(input_file);
+  this->polarizability = powerspectrum.get_polarizability();
+  this->greens_tensor = powerspectrum.get_greens_tensor();
 }
 
 Friction::Friction(std::shared_ptr<GreensTensor> greens_tensor,
